38afc039be89c0203377a20a54396c252f8327fa
av_elier <adelier1111@gmail.com>
2018-12-27 02:02:36 +0300

    implement continued fraction with tests

--- frac.go
+++ frac.go
@@ -1,9 +1,41 @@
 package dectofrac
 
-import "math/big"
+import (
+	"math"
+	"math/big"
+)
 
-// NewRat returns rational from decimal using iterations number of iterations in Continued Fraction algorythm
-func NewRat(val *big.Float, iterations int) *big.Rat {
-	res, _ := val.Rat(nil)
+// NewRatIterations returns rational from decimal
+// using `iterations` number of iterations in Continued Fraction algorythm
+func NewRatIterations(val float64, iterations int64) *big.Rat {
+	return newRat(val, iterations, 0)
+}
+
+// NewRatPrecision returns rational from decimal
+// by going as mush iterations, until next fraction is less than precision
+func NewRatPrecision(val float64, precision float64) *big.Rat {
+	return newRat(val, math.MaxInt64, precision)
+}
+
+func newRat(val float64, iterations int64, precision float64) *big.Rat {
+	a0 := int64(math.Floor(val))
+	x0 := val - float64(a0)
+	rat := cf(x0, 1, iterations, precision)
+	return rat.Add(rat, new(big.Rat).SetInt64(a0))
+}
+
+func cf(xi float64, i int64, limit int64, precision float64) *big.Rat {
+	if i >= limit || xi <= precision {
+		return big.NewRat(0, 1)
+	}
+
+	inverted := 1 / xi
+	aj := int64(math.Floor(inverted))
+	xj := inverted - float64(aj)
+	ratAJ := new(big.Rat).SetInt64(aj)
+	ratNext := cf(xj, i+1, limit, precision)
+	res := ratAJ.Add(ratAJ, ratNext)
+	res = res.Inv(res)
+
 	return res
 }
--- frac_test.go
+++ frac_test.go
@@ -1,27 +1,77 @@
 package dectofrac
 
 import (
-	"math/big"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewRat(t *testing.T) {
+func TestNewRatIterations(t *testing.T) {
 	type testCase struct {
 		expected   string
 		float      float64
-		iterations int
+		iterations int64
 	}
 	tests := []testCase{
 		{"1/4", 0.25, 4},
 		{"1/4", 0.25, 100},
-		{"2/3", 2 / 3.0, 6},
+		{"2/3", 2 / 3.0, 100},
+		{"2/3", 0.6666, 4},
+		{"3331/4997", 0.6666, 5},
+		{"2/3", 0.6667, 3},
+		{"6667/10000", 0.6667, 4},
 		{"1/7", 1 / 7.0, 7},
+		{"99/101", 99 / 101.0, 5},
+		{"108851651149874/111050674405427", 99 / 101.0, 6},
 	}
 	for _, test := range tests {
 		t.Run(test.expected, func(t *testing.T) {
-			assert.Equal(t, test.expected, NewRat(big.NewFloat(test.float), test.iterations).String())
+			assert.Equal(t, test.expected, NewRatIterations(test.float, test.iterations).String())
 		})
 	}
+}
+
+func TestNewRatPrecision(t *testing.T) {
+	type testCase struct {
+		expected  string
+		float     float64
+		precision float64
+	}
+	tests := []testCase{
+		{"1/4", 0.25, 1e-3},
+		{"1/4", 0.25, 1e-3},
+		{"2/3", 2 / 3.0, 1e-3},
+		{"2/3", 0.6666, 1e-3},
+		{"3333/5000", 0.6666, 1e-4},
+		{"2/3", 0.6667, 1e-3},
+		{"6667/10000", 0.6667, 1e-4},
+		{"1/7", 1 / 7.0, 1e-3},
+		{"99/101", 99 / 101.0, 1e-2},
+		{"99/101", 99 / 101.0, 1e-11},
+		{"108851651149874/111050674405427", 99 / 101.0, 1e-14},
+	}
+	for _, test := range tests {
+		t.Run(test.expected, func(t *testing.T) {
+			assert.Equal(t, test.expected, NewRatPrecision(test.float, test.precision).String())
+		})
+	}
+}
+
+func ExampleNewRatPrecision() {
+	fmt.Println(NewRatPrecision(0.6666, 0.01).String())
+	fmt.Println(NewRatPrecision(0.981, 0.001).String())
+	fmt.Println(NewRatPrecision(0.75, 0.01).String())
+	// Output:
+	// 2/3
+	// 981/1000
+	// 3/4
+}
+
+func ExampleNewRatIterations() {
+	fmt.Println(NewRatIterations(0.6667, 3).String())
+	fmt.Println(NewRatIterations(0.6667, 4).String())
+	// Output:
+	// 2/3
+	// 6667/10000
 }
